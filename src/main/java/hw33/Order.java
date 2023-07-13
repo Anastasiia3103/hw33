c8aa4607d2342bf7382689b1a326c1f6fcce2068
Anastasiia <lavrentjeva97@gmail.com>
2023-07-14 00:30:52 +0200

    Initial commit

---
+++ src/main/java/hw33/Order.java
@@ -0,0 +1,25 @@
+package hw33;
+
+public class Order {
+    private int orderNumber;
+    private String name;
+
+    public Order(int orderNumber, String name) {
+        this.orderNumber = orderNumber;
+        this.name = name;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return orderNumber + " | " + name;
+    }
+}
+
